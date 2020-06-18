a57b9dad8ae47106df00ecd00943e29af687acb9
Haymanot W <haymanotalemu21@gmail.com>
2020-06-18 07:22:22 -0400

    adding a question object in sccript

---
+++ script.js
@@ -0,0 +1,24 @@
+// Array of object question variable.
+var questions = [{question:"Inside which HTML element do   we put the JavaScript?",
+choice1: "<script>",
+choice2: "<Javascript>",
+choice3:"<Js>",
+choice4:"<scripting>",
+answer: 1 },
+{
+    question:"What is the correct syntax for reffering to an external script",
+    choice1: "<script href='xxx.js'>",
+    choice2: "<script name ='xxx.js'>",
+    choice3:"<script src ='xxx.js'>",
+    choice4:"<script file ='xxx.js'>",
+    answer: 3 },
+    {
+        question:"How do you write 'Hello World' in alert box?",
+        choice1: "msgBox('Hello World');",
+        choice2: "alertBox('Hello World');",
+        choice3:"msg('Hello World');",
+        choice4:"alert('Hello World');",
+        answer: 4 
+    }  
+
+]
