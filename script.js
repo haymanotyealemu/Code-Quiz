7bfe6c0e840cf49dbf303be68431de616abd5266
Haymanot W <haymanotalemu21@gmail.com>
2020-06-18 18:12:33 -0400

    hard script

--- script.js
+++ script.js
@@ -1,42 +1,109 @@
-// Array of object question variable.
-var questions = [{question:"Inside which HTML element do   we put the JavaScript?",
-choice1: "<script>",
-choice2: "<Javascript>",
-choice3:"<Js>",
-choice4:"<scripting>",
-answer: 1 },
+// Array of object Questions and Answers stored in a variable.
+var questions = [{quiz:"Inside which HTML element do   we put the JavaScript?",
+    choices: ["<script>","<Javascript>","<Js>","<scripting>"],
+answer: "<script>" },
 {
-    question:"What is the correct syntax for reffering to an external script",
-    choice1: "<script href='xxx.js'>",
-    choice2: "<script name ='xxx.js'>",
-    choice3:"<script src ='xxx.js'>",
-    choice4:"<script file ='xxx.js'>",
-    answer: 3 },
-    {
-        question:"How do you write 'Hello World' in alert box?",
-        choice1: "msgBox('Hello World');",
-        choice2: "alertBox('Hello World');",
-        choice3:"msg('Hello World');",
-        choice4:"alert('Hello World');",
-        answer: 4 
-    },
-    {
-        question: "Commonly used data types DO NOT include:",
-        choice1: "strings", 
-        choice2: "booleans", 
-        choice3: "alerts", 
-        choice4:"numbers",
-        answer: 3
-    },
-    {
-        question: "Which built-in method combines the text of two strings and returns a new string?",
-        choice1: "append()", 
-        choice2: "concat()", 
-        choice3: "attach()", 
-        choice4:"None of the above.",
-        answer: 2
-    }
+    quiz:"What is the correct syntax for reffering to an external script",
+    choices: ["<script href='xxx.js'>","<script name ='xxx.js'>","<script src ='xxx.js'>","<script file ='xxx.js'>"],
+    answer: "<script src ='xxx.js'>" },
+{
+        quiz:"How do you write 'Hello World' in alert box?",
+        choices: ["msgBox('Hello World');","alertBox('Hello World');","msg('Hello World');","alert('Hello World');"],
+        answer: "alert('Hello World')"
+},
+{
+        quiz: "Commonly used data types DO NOT include:",
+        choices: ["strings","booleans","alerts","numbers",], 
+        answer: "alerts"
+},
+{
+        quiz: "Which built-in method combines the text of two strings and returns a new string?",
+        choices: ["append()","concat()", "attach()","None of the above."],
+        answer: "concat()"
+}
 
 ];
 // Dom Manipulation
-var startQuiz = document.get
+var sectionEl = document.getElementById("main-content");
+var divEl = document.getElementById("container");
+var startQuizbtn = document.getElementById("my-submit");
+var timerEl = document.getElementById("time-span");
+
+// Variable Declaration
+var questionDiv = document.createElement("div");
+var secondsLeft = 20*questions.length; // Given that 20 second for each question.
+var score = 0;
+var index = 0;
+
+//set time for questions.
+function setTime(){
+    var timeInterval = setInterval(function(){
+    if(secondsLeft > 0){
+        secondsLeft--;
+    }
+    else{
+        secondsLeft = 0;
+    }
+        timerEl = secondsLeft;
+    if(secondsLeft === 0){
+        clearInterval(timeInterval);
+        timerEl = secondsLeft;
+        emptyQuiz2(questionDiv);
+        finalScore();
+    }
+    },1000)
+}
+
+var scores=[];
+//this is the object in which I store my players.
+var players={
+    name:"",
+    score:0,
+};
+
+ //This function removes all the children of el
+function emptyQuiz2(el){
+
+    while (el.firstChild) {
+        el.removeChild(el.firstChild);
+    } 
+}
+
+//Shows the questions on the page
+function showQuestion(){
+    if(index >= questions.length)
+    { finalScore();
+        return;}
+    var question=document.createElement("p");
+    question.textContent=questions[index].quiz;
+    questionDiv.append(question);
+    questionDiv.setAttribute("style","display:block");
+    sectionEl.append(questionDiv);
+    answerChoices(); //lists the options
+}
+
+// Show answer choices on the page and event deligation to lists.
+function answerChoices(){
+    var choices = document.createElement("ol");
+    choices.textContent = 
+}
+
+
+// Render question function
+
+let lastquestionIndex = questions.length - 1;
+let runningquestionIndex = 0;
+function renderQuestion(){
+    
+
+}
+// Start Quiz button. Here our start quiz button handles a click event.
+startQuizbtn.addEventListener("click", function(event){
+    event.preventDefault();
+    var questionDiv = document.createElement("div");
+    questionDiv.textContent = (questions[i]);
+    divEl.appendChild(questionDiv);
+
+
+});
+
